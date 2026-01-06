58766b1023e96cb6a2af014da29c6b4986f50299
Ahmed Ahlaqqach <aahlaqqa@Ahmed.Inception>
2026-01-07 00:51:38 +0100

    Added exclusive page

--- netpong-app/src/App.jsx
+++ netpong-app/src/App.jsx
@@ -4,10 +4,14 @@ import Home from './pages/Home';
 import Login from './pages/login';
 import SignUp from './pages/signup';
 import SaulSociety from './pages/saul-society';
+import ZombieLand from './pages/zombie-land';
+import Joker from './pages/joker';
+import BarbiePink from './pages/barbie-pink';
+import Exclusive from './pages/exclusive';
 
 function Navigation() {
   const location = useLocation();
-  const hideHeaderPaths = ['/login', '/signup', '/saul-society'];
+  const hideHeaderPaths = ['/login', '/signup', '/saul-society', '/zombie-land', '/joker', '/barbie-pink'];
   if (hideHeaderPaths.includes(location.pathname)) {
     return null;
   }
@@ -28,6 +32,10 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/saul-society" element={<SaulSociety />} />
+            <Route path="/zombie-land" element={<ZombieLand />} />
+            <Route path="/joker" element={<Joker />} />
+            <Route path="/barbie-pink" element={<BarbiePink />} />
+            <Route path="/exclusive" element={<Exclusive />} />
           </Routes>
         </main>
       </div>
