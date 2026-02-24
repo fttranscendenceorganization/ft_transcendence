a2e9869e39f99b6ee65d3e7bb4ecb6ad26d319ec
Houdaifa Drahm <houdaifadrahm@gmail.com>
2026-02-24 21:40:28 +0000

    hdrahm<feb 24>: protect the routes

--- netpong-app/src/utils/PrivateRoute.jsx
+++ netpong-app/src/utils/PrivateRoute.jsx
@@ -1,7 +1,9 @@
 import { Navigate } from 'react-router-dom';
 import { getToken } from './authToken';
+import { refreshAccessToken } from './api';
 
 export default function PrivateRoute({ children }) {
+    refreshAccessToken();
   const token = getToken();
   if (!token) {
     return <Navigate to="/login" replace />;
