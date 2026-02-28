da116a7828565d00fd70354b7885492ad8c6514b
Houdaifa Drahm <houdaifadrahm@gmail.com>
2026-02-28 09:17:05 +0000

    hdrahm<feb 28>: adding the fetch call for game history

--- backend_srcs/src/game/types/game-mode.enum.ts
+++ backend_srcs/src/game/types/game-mode.enum.ts
@@ -2,6 +2,6 @@ export enum GameModeEnum
 {
     SOUL_SOCIETY = 'SOUL_SOCIETY',
     ZOMBIE_LAND = 'ZOMBIE_LAND',
-    KITTY_CAT    = 'KITTY_CAT',
+    BARBIE_PINK = 'BARBIE_PINK',
     JOKER = 'JOKER'
 }
