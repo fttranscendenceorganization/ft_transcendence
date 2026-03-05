cae39533453a45c5cab9adf1c7057e65c8ac302d
Houdaifa Drahm <houdaifadrahm@gmail.com>
2026-03-05 20:50:07 +0000

    hdrahm<mar 5>: adding ball speed

--- backend_srcs/src/game/game.service.ts
+++ backend_srcs/src/game/game.service.ts
@@ -211,7 +211,7 @@ export class GameService implements OnModuleDestroy {
         });
 
         const currentSpeed = Math.sqrt(ballVelocity.x * ballVelocity.x + ballVelocity.y * ballVelocity.y);
-        const MIN_BALL_SPEED = 6;
+        const MIN_BALL_SPEED = 10;
         if (currentSpeed > 0.5 && currentSpeed < MIN_BALL_SPEED) {
             const scale = MIN_BALL_SPEED / currentSpeed;
             ballVelocity.x *= scale;
