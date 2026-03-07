8337903c921dc3b35e2b35df726ec48e194c8098
doffy <ysfad2628@gmail.com>
2026-03-07 04:20:29 +0000

    UPDATE: fix ahmed link + added houdaifa to contact page

--- netpong-app/src/pages/FirstContact.jsx
+++ netpong-app/src/pages/FirstContact.jsx
@@ -13,7 +13,7 @@ export default function FirstContact() {
             role: 'Frontend Developer',
             initial: 'A',
             image: 'https://media.licdn.com/dms/image/v2/D4E03AQHYREm74qVPHw/profile-displayphoto-shrink_800_800/B4EZP0OiZxHgAk-/0/1734969272840?e=1774483200&v=beta&t=29leJg_lW-4RLQvBymkLNglZiJj90neviWBJFgvApfQ',
-            linkedin: 'https://www.linkedin.com/in/ahmed-ahlaqqach-b1695719a'
+            linkedin: 'https://www.linkedin.com/in/ahmed-ahlaqqach-b1695719a',
             bgColor: 'bg-red-700',
             btnHover: 'hover:bg-red-600',
             hoverBorder: 'hover:border-red-700/50'
@@ -22,8 +22,8 @@ export default function FirstContact() {
             name: 'Houdaifa',
             role: 'Backend Developer',
             initial: 'H',
-            image: 'https://i.pravatar.cc/200?img=12',
-            linkedin: 'random linked in',
+            image: 'https://media.licdn.com/dms/image/v2/D4E03AQFRtzYluWoEzg/profile-displayphoto-crop_800_800/B4EZyaQ2hwJsAQ-/0/1772114636386?e=1774483200&v=beta&t=se6ao9eUSCgGlbZg9msB5GqtmvzaaCfdUgXfhmod7kM',
+            linkedin: 'https://www.linkedin.com/in/houdaifa-drahm/',
             bgColor: 'bg-orange-500',
             btnHover: 'hover:bg-orange-400',
             hoverBorder: 'hover:border-orange-500/50'
